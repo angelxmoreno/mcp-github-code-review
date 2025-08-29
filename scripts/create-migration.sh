6fede4be6374ac522464d67b504402078f44b039
Angel S. Moreno <angelxmoreno@gmail.com>
2025-08-29 12:36:09 -0400

    fix: fixed migrations path for create migration script

--- scripts/create-migration.sh
+++ scripts/create-migration.sh
@@ -3,4 +3,4 @@ if [ -z "$1" ]; then
   echo "Usage: bun migration:create <MigrationName>"
   exit 1
 fi
-bun --bun typeorm migration:create --esm "src/migrations/$1"
+bun --bun typeorm migration:create --esm "src/database/migrations/$1"
